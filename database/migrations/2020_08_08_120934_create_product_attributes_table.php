d191b2eb58190c731f622bf95919f71201a63cde
Amir <zpr.amir@gmail.com>
2020-08-08 16:46:57 +0430

    add product_attributes pivot table migration

---
+++ database/migrations/2020_08_08_120934_create_product_attributes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->foreignId('attribute_item_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+
+            $table->primary(['attribute_item_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_product');
+    }
+}
