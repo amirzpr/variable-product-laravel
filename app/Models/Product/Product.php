78e9a78f4f6b551d7def79cfa567d7acde10a53c
Amir <zpr.amir@gmail.com>
2020-08-12 02:10:05 +0430

    fix Product model relationships

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -2,16 +2,14 @@
 
 namespace App\Models\Product;
 
-use App\Models\Product\Attribute\Attribute;
+use App\Models\Product\Attribute\AttributeOption;
 use App\Models\Product\Attribute\BooleanAttributeValue;
-use App\Models\Product\Attribute\MultiSelectableAttributeValue;
-use App\Models\Product\Attribute\SelectableAttributeValue;
 use App\Models\Product\Attribute\TextAttributeValue;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property Category rootCategory
-* @mixin \Eloquent
+ * @mixin \Eloquent
 */
 class Product extends Model
 {
@@ -32,19 +30,35 @@ class Product extends Model
         return $this->hasMany(BooleanAttributeValue::class);
     }
 
+    public function textAttributeValues()
+    {
+        return $this->hasMany(TextAttributeValue::class);
+    }
+
     public function selectableAttributeValues()
     {
-        return $this->hasMany(SelectableAttributeValue::class);
+        return $this->belongsToMany(AttributeOption::class, 'selectable_attribute_values',
+            'product_id', 'option_id');
     }
 
     public function multiSelectableAttributeValues()
     {
-        return $this->belongsToMany(MultiSelectableAttributeValue::class, 'multi_selectable_attribute_values',
+        return $this->belongsToMany(AttributeOption::class, 'multi_selectable_attribute_values',
             'product_id', 'option_id');
     }
 
-    public function textAttributeValues()
+    /**
+     * Returns values of all attribute types in one collection
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function getAllAttributeValuesAttribute()
     {
-        return $this->hasMany(TextAttributeValue::class);
+        return collect([
+            $this->booleanAttributeValues,
+            $this->textAttributeValues,
+            $this->selectableAttributeValues,
+            $this->multiSelectableAttributeValues,
+        ])->flatten();
     }
 }
